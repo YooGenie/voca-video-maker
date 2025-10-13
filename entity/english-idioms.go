6fdbf9453eebc7e92b44159226e9bf9899ec7a25
sunny <yshee712@gmail.com>
2025-10-13 19:25:57 +0900

    Feature: 필요없는 컬럼 삭제

--- entity/english-idioms.go
+++ entity/english-idioms.go
@@ -7,7 +7,6 @@ type EnglishIdiom struct {
 	PronunciationKr   string `xorm:"pronunciation_kr"`
 	PhoneticSymbol    string `xorm:"phonetic_symbol"`
 	CreatedDate       string `xorm:"created_date"`
-	IsUploadedYoutube bool   `xorm:"is_uploaded_youtube"`
 }
 
 func (EnglishIdiom) TableName() string {
